41ca2e7f596035032918e79ecd6bc041b777f90c
lakshit <lakshit@lakshit-PC>
2018-02-04 12:56:04 +0530

    handle cookies first commit

---
+++ src/arrayprograms/Array1.java
@@ -0,0 +1,41 @@
+package arrayprograms;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		
+		int abc[][]={{2,1,5}, {3,0,7}, {1,2,9}};
+		int min=abc[0][0];
+		
+		int mincolumn=0;
+		
+		for(int i=0;i<3; i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				if(abc[i][j]<min)
+					
+				{
+					min=abc[i][j];
+	             	mincolumn=j;
+					
+				}
+				
+			}
+		}
+		
+               int k=0;
+               int max =abc[0][mincolumn];
+                while(k<3)
+               {
+            	   if(abc[k][mincolumn]>max)
+            	   {
+            		   max=abc[k][mincolumn];
+            		   
+            	   }
+            	   k++;
+               }
+            System.out.println(max);   
+	}
+
+}
---
+++ src/arrayprograms/Trycatchexception.java
@@ -0,0 +1,28 @@
+package arrayprograms;
+
+public class Trycatchexception {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int b=4;
+		int c=0;
+		try
+		{
+			int k=b/c;
+			
+		}
+		catch(ArithmeticException h)
+		{
+			System.out.println("i am a arthmetic error");
+		}
+	catch(Exception d)
+		{
+		System.out.println("i am a catched error");
+		}
+		
+		}
+		
+	
+
+}
